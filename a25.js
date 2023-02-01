3b20ba0fda1c1c5b12cb26ce454da1f489355c11
Shyam Ji Gupta <91778264+Shyamjizoyo@users.noreply.github.com>
2023-02-01 21:02:42 +0530

    Add files via upload

---
+++ a25.js
@@ -0,0 +1,5 @@
+//object literals in javascript
+detail = { name:'shyam', class:'1st', id:'001'};
+console.log(typeof(detail));
+console.log(detail["name"]); //1st method to access
+console.log(detail.class); //2nd method to access
