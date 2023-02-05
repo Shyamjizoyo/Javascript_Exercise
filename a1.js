9cfe2de514a637106b2978fc28684dd4195dbf59
Shyam Ji Gupta <91778264+Shyamjizoyo@users.noreply.github.com>
2023-02-06 02:00:22 +0530

    Update a1.js

--- a1.js
+++ a1.js
@@ -2,6 +2,6 @@ var a = 5;
 var b = 5;
 console.log(a==b)
 console.log(" is both same: " + b==a ); 
-// compiler thinks we are concating the opertaion
+// compiler thinks we are concating the opertaion we can use bracets to avoid this
 console.log(`is both same: ${b==a}`);
 
